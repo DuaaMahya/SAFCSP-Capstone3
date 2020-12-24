404e18e4d4fb2a3f5c4511728848c9ae12d7f276
DuaaMahya <temoka48@gmail.com>
2020-12-24 13:33:41 +0300

    done with main page and business detail page

---
+++ Tourme/Tourme/Controller/ViewController.swift
@@ -0,0 +1,314 @@
+//
+//  ViewController.swift
+//  Tourme
+//
+//  Created by Dua Almahyani on 21/12/2020.
+//
+
+import UIKit
+
+private let tableCell = "businessesCell"
+private let collectionCell = "categoriesCell"
+
+class ViewController: UIViewController {
+    
+    var searchBar = UISearchBar()
+    var tableView = UITableView()
+    var collectionView: UICollectionView?
+    let utility = Utilities()
+    let images: [UIImage] = [#imageLiteral(resourceName: "ResturantIcon"), #imageLiteral(resourceName: "RealEstateIcon"), #imageLiteral(resourceName: "HomeServiceIcon"), #imageLiteral(resourceName: "EducationIcon"), #imageLiteral(resourceName: "PetsIcon"), #imageLiteral(resourceName: "ArtIcon"), #imageLiteral(resourceName: "EventPlaningIcon"), #imageLiteral(resourceName: "more+")]
+    let categories = ["Restaurants", "Real Estate", "Home Service", "Education", "Pets", "Art","Event Planning"," "]
+    
+    lazy var header: UIView = {
+        let view = UIView(frame: CGRect(x: 0, y: 0, width: self.view.frame.size.width, height: self.view.frame.size.width + 40))
+        
+        
+        view.addSubview(cityLabel)
+        cityLabel.anchor(top: view.topAnchor,
+                         left: view.leftAnchor,
+                         paddingTop: 20,
+                         paddingLeft: 20,
+                         paddingBottom: 20)
+        
+        view.addSubview(containerView)
+        containerView.anchor(top: cityLabel.bottomAnchor, left: view.leftAnchor, right: view.rightAnchor, paddingTop: 20, paddingLeft: 20, paddingRight: 20, height: 227)
+        
+        
+        view.addSubview(containerButton)
+        containerButton.anchor(top: cityLabel.bottomAnchor, left: view.leftAnchor, right: view.rightAnchor, paddingTop: 20, paddingLeft: 20, paddingRight: 20, height: 227)
+        
+        
+        return view
+    }()
+    
+    lazy var cityLabel: UILabel = {
+        let label = UILabel()
+        label.text = "City"
+        label.font = UIFont.boldSystemFont(ofSize: 33)
+        return label
+    }()
+    
+    lazy var containerButton: UIButton = {
+        let button = UIButton(frame: CGRect(x: 0, y: 0, width: self.view.frame.size.width - 40, height: 227))
+        button.addTarget(self, action: #selector(weatherViewTapped), for: .allEvents)
+        return button
+    }()
+    
+    lazy var containerView: UIView = {
+        let view = UIView(frame: CGRect(x: 0, y: 0, width: self.view.frame.size.width - 40, height: 227))
+        view.layer.cornerRadius = 9
+        view.layer.shadowColor = UIColor.darkGray.cgColor
+        view.layer.shadowRadius = 7
+        view.layer.shadowOpacity = 0.3
+        view.layer.shadowOffset = CGSize(width: 0, height: 10)
+        
+        view.addSubview(weatherConditionImage)
+        weatherConditionImage.anchor(top: view.topAnchor,
+                                     right: view.rightAnchor,
+                                     paddingTop: 18,
+                                     paddingRight: 8,
+                                     width: 80,
+                                     height: 80)
+        
+        view.addSubview(weatherImage)
+        weatherImage.anchor(bottom: view.bottomAnchor,
+                            right: view.rightAnchor,
+                            paddingBottom: 8,
+                            paddingRight: 40,
+                            width: 124,
+                            height: 124)
+        
+        view.addSubview(weatherStackView)
+        weatherStackView.centerYAnchor.constraint(equalTo: view.centerYAnchor).isActive = true
+        weatherStackView.anchor(left: view.leftAnchor, paddingLeft: 32, width: 110, height: 154)
+        
+        return view
+    }()
+    
+    lazy var dayStackView: UIStackView = {
+        let stack = UIStackView()
+        stack.axis = .vertical
+        stack.alignment = .center
+        stack.distribution = .fillEqually
+        stack.spacing = -14
+        stack.layer.cornerRadius = 9.0
+        
+        stack.addArrangedSubview(dayLabel)
+        stack.addArrangedSubview(dateLabel)
+        
+        
+        return stack
+    }()
+    
+    lazy var weatherStackView: UIStackView = {
+        let stack = UIStackView()
+        stack.axis = .vertical
+        stack.alignment = .center
+        stack.distribution = .fillProportionally
+        stack.layer.cornerRadius = 9.0
+        stack.backgroundColor = UIColor(named: "Color")
+        
+        stack.addArrangedSubview(tempertureLabel)
+        stack.addArrangedSubview(dayStackView)
+        
+        return stack
+    }()
+    
+    let weatherConditionImage: UIImageView = {
+        let image = UIImageView()
+        image.image = #imageLiteral(resourceName: "sun")
+        image.contentMode = .scaleAspectFit
+        return image
+    }()
+    
+    let weatherImage: UIImageView = {
+        let image = UIImageView()
+        image.image = #imageLiteral(resourceName: "Saly-22")
+        return image
+    }()
+    
+    var tempertureLabel: UILabel = {
+        var label = UILabel()
+        label.text = "3C"
+        label.font = UIFont.boldSystemFont(ofSize: 32)
+        return label
+    }()
+    
+    var dayLabel: UILabel = {
+        var label = UILabel()
+        label.text = "Today"
+        label.font = UIFont.boldSystemFont(ofSize: 14)
+        return label
+    }()
+    
+    var dateLabel: UILabel = {
+        var label = UILabel()
+        label.text = "3/12/2020"
+        label.font = UIFont.systemFont(ofSize: 10)
+        return label
+    }()
+    
+    //MARK: - Override
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        utility.blur(view: view)
+        
+        utility.gradentColors(color1: Colors.lightGreen.cgColor, color2: Colors.green.cgColor, view: containerView, cornerRadius: 9)
+        
+        setupSearchBar()
+        setupTableView()
+        setupCollectionView()
+    }
+    
+    //MARK: - Functions
+    
+    func setupSearchBar() {
+        searchBar.delegate = self
+        searchBar.placeholder = "Search"
+        searchBar.searchBarStyle = .minimal
+        
+       // utility.blur(view: searchBar.backgroundColor.cgColor)
+        self.view.addSubview(searchBar)
+        searchBar.anchor(top: view.safeAreaLayoutGuide.topAnchor,
+                         left: view.leftAnchor,
+                         right: view.rightAnchor,
+                         paddingTop: 20,
+                         paddingLeft: 20,
+                         paddingRight: 20)
+    }
+    
+    func setupTableView() {
+        tableView.delegate = self
+        tableView.dataSource = self
+        tableView.tableHeaderView = header
+        self.view.addSubview(tableView)
+        tableView.backgroundColor = UIColor(named: "BGColor")
+        tableView.separatorColor = .clear
+        tableView.rowHeight = 90
+        tableView.register(mainTableViewCell.self, forCellReuseIdentifier: tableCell)
+        tableView.anchor(top: searchBar.bottomAnchor, left: view.leftAnchor,
+                         bottom: view.bottomAnchor, right: view.rightAnchor,
+                         paddingTop: 10, paddingLeft: 0,
+                         paddingBottom: 0, paddingRight: 0)
+        tableView.reloadData()
+    }
+    
+    func setupCollectionView() {
+        let layout = UICollectionViewFlowLayout()
+        layout.scrollDirection = .horizontal
+        layout.sectionInset.left = 20
+        layout.sectionInset.right = 20
+        collectionView = UICollectionView(frame: .zero, collectionViewLayout: layout)
+        collectionView?.backgroundColor = .clear
+        collectionView?.showsHorizontalScrollIndicator = false
+        
+        guard let collectionView = collectionView else { return }
+        
+        collectionView.delegate = self
+        collectionView.dataSource = self
+    
+        collectionView.register(MainPageCategoriesCollectionViewCell.self, forCellWithReuseIdentifier: collectionCell)
+        
+        header.addSubview(collectionView)
+        
+        collectionView.anchor(top: containerView.bottomAnchor,
+                              left: header.leftAnchor,
+                              bottom: header.bottomAnchor,
+                              right: header.rightAnchor, paddingTop: 8)
+    }
+    
+    //MARK: - @objc Selectors
+    
+    @objc func weatherViewTapped(_ sender: UIButton) {
+        performSegue(withIdentifier: "weatherVC", sender: nil)
+    }
+    
+
+
+}
+
+extension ViewController: UISearchBarDelegate {
+    func searchBarCancelButtonClicked(_ searchBar: UISearchBar) {
+        searchBar.text = ""
+        searchBar.endEditing(true)
+    }
+    
+    func searchBarShouldEndEditing(_ searchBar: UISearchBar) -> Bool {
+        searchBar.endEditing(true)
+    }
+    
+    func searchBarTextDidEndEditing(_ searchBar: UISearchBar) {
+        searchBar.endEditing(true)
+    }
+    
+    func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
+        //print("search = \(searchText)")
+    }
+}
+
+extension ViewController: UITableViewDelegate, UITableViewDataSource {
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath, animated: true)
+    }
+    
+    func tableView(_ tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat { 40 }
+    
+    func numberOfSections(in tableView: UITableView) -> Int { 1 }
+    
+    func tableView(_ tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? {
+        let view = UIView(frame: CGRect(x: 0, y: 0, width: tableView.frame.width, height: 40))
+        //view.backgroundColor = UIColor(red: 1/255, green: 1/255, blue: 1/255, alpha: 0.3)
+        
+        utility.blur(view: view)
+        
+        let label = UILabel(frame: CGRect(x: 15, y: 0, width: view.frame.width-15, height: 40))
+        label.font = UIFont.boldSystemFont(ofSize: 24)
+        label.text = "Businesses near me"
+        view.addSubview(label)
+        return view
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        13
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: tableCell, for: indexPath) as! mainTableViewCell
+        cell.textLabel?.text = "test"
+        return cell
+    }
+    
+    
+}
+
+extension ViewController: UICollectionViewDelegate, UICollectionViewDataSource {
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        images.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: collectionCell, for: indexPath) as! MainPageCategoriesCollectionViewCell
+        let itemImage = images[indexPath.row]
+        let itemLabel = categories[indexPath.row]
+        cell.categoryImage.image = itemImage
+        cell.categoryNameLabel.text = itemLabel
+        return cell
+    }
+    
+}
+
+extension ViewController: UICollectionViewDelegateFlowLayout {
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumInteritemSpacingForSectionAt section: Int) -> CGFloat {
+        return 3
+    }
+    
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        return CGSize(width: 63, height: 85)
+    }
+}
---
+++ Tourme/Tourme/View/mainTableViewCell.swift
@@ -0,0 +1,183 @@
+//
+//  mainTableViewCell.swift
+//  Tourme
+//
+//  Created by Dua Almahyani on 21/12/2020.
+//
+
+import UIKit
+
+class mainTableViewCell: UITableViewCell {
+    
+    let businessImage: UIImageView = {
+        let image = UIImageView(frame: CGRect(x: 0, y: 0, width: 84, height: 66))
+        image.image = #imageLiteral(resourceName: "a42g7clq33q51")
+        image.contentMode = .scaleAspectFill
+        image.layer.masksToBounds = true
+        image.layer.cornerRadius = 9
+        return image
+    }()
+    
+    let weatherImage: UIImageView = {
+        let image = UIImageView()
+        image.image = UIImage(systemName: "cloud")
+        image.tintColor = .white
+        image.layer.shadowColor = UIColor.black.cgColor
+        image.layer.shadowRadius = 7
+        image.layer.shadowOpacity = 0.3
+        image.layer.shadowOffset = CGSize(width: 0, height: 5)
+        return image
+    }()
+    
+    let businessNameLabel: UILabel = {
+        let label = UILabel()
+        label.text = "Kindom Centre"
+        label.font = UIFont.boldSystemFont(ofSize: 13)
+        return label
+    }()
+    
+    let businessCategoryLabel: UILabel = {
+        let label = UILabel()
+        label.text = "Local Flavor"
+        label.font = UIFont.systemFont(ofSize: 10)
+        return label
+    }()
+
+    let businessCurrntStateLabel: UILabel = {
+        let label = UILabel()
+        label.text = "open"
+        label.font = UIFont.systemFont(ofSize: 10)
+        label.textColor = UIColor(red: 4/255, green: 168/255, blue: 1/255, alpha: 1)
+        return label
+    }()
+    
+    let starImage: UIImageView = {
+        let image = UIImageView()
+        image.image = #imageLiteral(resourceName: "smallStar")
+        image.contentMode = .scaleAspectFill
+        image.clipsToBounds = true
+        image.layer.borderColor = UIColor.white.cgColor
+        return image
+    }()
+    
+    let unfilledStarImage: UIImageView = {
+        let image = UIImageView()
+        image.image = #imageLiteral(resourceName: "UnfilledStar")
+        image.contentMode = .scaleAspectFill
+        image.clipsToBounds = true
+        image.layer.borderColor = UIColor.white.cgColor
+        return image
+    }()
+    
+    var numberOfStars = 5
+
+    lazy var starsStackView: UIStackView = {
+        let stack = UIStackView()
+        stack.axis = .horizontal
+        stack.alignment = .fill
+        stack.distribution = .fill
+        stack.spacing = 1
+        
+        starImage.anchor(width: 5, height: 5)
+        stack.addArrangedSubview(starImage)
+        stack.addArrangedSubview(starImage)
+        stack.addArrangedSubview(starImage)
+        stack.addArrangedSubview(starImage)
+        stack.addArrangedSubview(starImage)
+        
+        return stack
+    }()
+    
+    lazy var statusStackView: UIStackView = {
+        let stack = UIStackView()
+        stack.axis = .horizontal
+        stack.alignment = .fill
+        stack.distribution = .fill
+        stack.spacing = 0
+        
+        stack.addArrangedSubview(businessCurrntStateLabel)
+        stack.addArrangedSubview(starsStackView)
+        
+        return stack
+    }()
+    
+    lazy var InfoStackView: UIStackView = {
+        let stack = UIStackView()
+        stack.axis = .vertical
+        stack.alignment = .leading
+        stack.distribution = .fillEqually
+        
+        
+        stack.addArrangedSubview(businessNameLabel)
+        stack.addArrangedSubview(businessCategoryLabel)
+        stack.addArrangedSubview(statusStackView)
+        
+        
+        
+        return stack
+    }()
+    
+    lazy var backgroundColorView: UIView = {
+        let view = UIView()
+        view.layer.cornerRadius = 9
+        view.backgroundColor = UIColor.init(red: 0, green: 0, blue: 0, alpha: 0.07)
+        view.clipsToBounds = false
+        view.layer.shadowColor = UIColor.black.cgColor
+        view.layer.shadowRadius = 7
+        view.layer.shadowOpacity = 1
+        view.layer.shadowOffset = CGSize(width: 0, height: 10)
+        
+        
+        return view
+    }()
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        addSubview(businessImage)
+        businessImage.anchor(top: topAnchor, left: leftAnchor, bottom: bottomAnchor, paddingTop: 15, paddingLeft: 20, paddingBottom: 15, width: 84, height: 66)
+        
+        addSubview(backgroundColorView)
+        backgroundColorView.anchor(top: topAnchor, left: leftAnchor, bottom: bottomAnchor, right: rightAnchor, paddingTop: 18, paddingLeft: 20, paddingBottom: 18, paddingRight: 20)
+        
+        addSubview(InfoStackView)
+        InfoStackView.anchor(top: topAnchor, left: businessImage.rightAnchor, bottom: bottomAnchor, right: rightAnchor, paddingTop: 25, paddingLeft: 10, paddingBottom: 25, paddingRight: 20)
+        
+        addSubview(weatherImage)
+        weatherImage.centerYAnchor.constraint(equalTo: backgroundColorView.centerYAnchor).isActive = true
+        weatherImage.anchor(right: rightAnchor,
+                            paddingRight: 30,
+                            width: 30, height: 30)
+        
+    }
+
+    func businessStar(numberOfStars: Int) {
+        
+        for _ in 1...numberOfStars {
+            let image = UIImageView()
+            image.image = #imageLiteral(resourceName: "Star")
+            image.contentMode = .scaleAspectFill
+            image.clipsToBounds = true
+            image.layer.borderColor = UIColor.white.cgColor
+            starsStackView.addArrangedSubview(image)
+        }
+        
+//       if numberOfStars < 5 {
+//            let unfilledStars = 5 - numberOfStars
+//            for _ in 1...unfilledStars {
+//                let image = UIImageView(frame: CGRect(x: 0, y: 0, width: 15, height: 15))
+//                 image.image = #imageLiteral(resourceName: "UnfilledStar")
+//                image.contentMode = .scaleAspectFill
+//                image.clipsToBounds = true
+//                image.layer.borderColor = UIColor.white.cgColor
+//                starsStackView.addArrangedSubview(image)
+//            }
+//        }
+    }
+    
+}
