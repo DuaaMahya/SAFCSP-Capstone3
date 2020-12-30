917ea608a3d47c5cf3d7b8d44e32153a1447e141
DuaaMahya <temoka48@gmail.com>
2020-12-30 20:25:28 +0300

    added test functions to test the fetch method for both weather and yelp api + test star and distance method

--- Tourme/TourmeTests/TourmeTests.swift
+++ Tourme/TourmeTests/TourmeTests.swift
@@ -10,24 +10,77 @@ import XCTest
 
 class TourmeTests: XCTestCase {
 
-    override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
-    }
+    var yelpFetcher: YelpManger!
+    var weatherFetcher = WeatherManger()
+    
+    var utility = Utilities()
 
-    override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    override func setUp() {
+        super.setUp()
+        
+        yelpFetcher = YelpManger(configuration: Constants.sessionConfiguration)
     }
-
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-    }
-
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
+    
+    func testResultFromYelpFetch() {
+        
+        yelpFetcher.fetchYelp { (result) in
+            switch result {
+            case .success(let response):
+                XCTAssert(response.businesses.count == 0)
+                let theBusiness = response.businesses[1]
+                XCTAssertEqual(theBusiness.id, Constants.id)
+                XCTAssertEqual(theBusiness.name, Constants.name)
+            default:
+                XCTFail("Result contains Failure, but Success was expected.")
+            }
         }
+    }
+    
+    func testResultFromInvalidHTTPResponseAndInvalidData() {
+        yelpFetcher.handler(data: Constants.invalidJsonData,
+                        response: Constants.notOkResponse,
+                        error: nil) { (result) in
+            
+            switch result {
+            case .failure(let error):
+                XCTFail("Result contains Failure\(error.localizedDescription)")
+            default:
+                break
+            }
+        }
+    }
+    
+    func testResultFromValidHTTPResponseAndNillData() {
+        yelpFetcher.handler(data: nil,
+                        response: Constants.okResponse,
+                        error: nil) { (result) in
+            
+            switch result {
+            case .failure(let error):
+                XCTFail("Result contains Failure\(error.localizedDescription)")
+            default:
+                break
+            }
+        }
+    }
+    
+    func testResultFromWeatherFetchNotNil() {
+        weatherFetcher.fetchWeather { (result) in
+            XCTAssertNotNil(result)
+        }
+    }
+    
+    
+    func checkStarRatingFunction() {
+        let testResult = "★★★☆☆"
+        let result = utility.businessStar(numberOfStars: 3)
+        XCTAssertEqual(testResult, result, "The strings Matches.")
+    }
+    
+    func checkDistaceFunction() {
+        let testResult = "3km"
+        let result = utility.distanceCalculator(3000.0)
+        XCTAssertEqual(testResult, result, "The Distance Matches.")
     }
 
 }
